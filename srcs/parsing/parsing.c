44c0b6236d94b51a0aaf8cc2d375799f44130218
yzidani <yzidani@f2r8s17.paris.42.school>
2026-02-28 17:28:44 +0100

    parsing.c ++

---
+++ srcs/parsing/parsing.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yzidani <yzidani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/28 17:01:13 by yzidani           #+#    #+#             */
+/*   Updated: 2026/02/28 17:28:22 by yzidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+t_cmd	*parsing(t_data *data)
+{
+    t_token	*token_list;
+    t_cmd	*cmd_list;
+
+    token_list = tokenize(data);
+    if (!token_list)
+        return (NULL);
+    cmd_list = parse_tokens(token_list);
+    free_tokens(token_list);
+    return (cmd_list);
+}
